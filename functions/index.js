758048a943741692cf45c0f3e81342409b458b24
azukiazusa1 <cycleippei@gmail.com>
2020-04-26 18:42:07 +0900

    記事が変更されたときアーカイブをカウントします

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const moment = require('moment')
 admin.initializeApp()
 
 const firestore = admin.firestore()
 const tagasRef = firestore.collection('tags')
+const archivesRef = firestore.collection('archives')
 
 exports.updateArticle = functions.firestore
   .document('articles/{articleId}')
@@ -23,18 +25,27 @@ exports.updateArticle = functions.firestore
               }, { merge: true })
           })
       })
-      // 公開から下書きに変更されたとき
-    } else if (!previousData.published && data.published) {
-      data.tags.forEach(tag => {
-        tagasRef.doc(tag)
-          .get()
-          .then(querySnapshot => {
-            tagasRef.doc(tag)
-              .update({ 
-                articleCount: --querySnapshot.data().articleCount
+
+      const createdMonth = moment(data.created.seconds * 1000).format('YYYY-MM')
+      
+      archivesRef.doc(createdMonth)
+        .get()
+        .then(querySnapshot => {
+          if (querySnapshot.exists) {
+            archivesRef.doc(createdMonth)
+              .update({
+                articleCount: ++querySnapshot.data().articleCount
               }, { merge: true })
-          })
-      })
+          } else {
+            archivesRef.doc(createdMonth)
+              .set({
+                articleCount: 1
+              })
+          }
+        })
+      // 公開から下書きに変更されたとき
+    } else if (previousData.published && !data.published) {
+      deleteEvent(data)
     } else {
       return
     }
@@ -45,14 +56,33 @@ exports.deleteArticls = functions.firestore
   .onDelete(snap => {
     const data = snap.data()
     if (!data.published) return
-    data.tags.forEach(tag => {
-      tagasRef.doc(tag)
-        .get()
-        .then(querySnapshot => {
-          tagasRef.doc(tag)
-            .update({ 
+    deleteEvent(data)
+  })
+
+function deleteEvent(data) {
+  data.tags.forEach(tag => {
+    tagasRef.doc(tag)
+      .get()
+      .then(querySnapshot => {
+        tagasRef.doc(tag)
+          .update({ 
+            articleCount: --querySnapshot.data().articleCount
+          }, { merge: true })
+      })
+  })
+  const createdMonth = moment(data.created.seconds * 1000).format('YYYY-MM')
+  archivesRef.doc(createdMonth)
+    .get()
+    .then(querySnapshot => {
+      if (querySnapshot.exists) {
+        if (querySnapshot.data().articleCount > 1) {
+          archivesRef.doc(createdMonth)
+            .update({
               articleCount: --querySnapshot.data().articleCount
             }, { merge: true })
-        })
+        } else {
+          archivesRef.doc(createdMonth).delete()
+        }
+      }
     })
-  })
+}
